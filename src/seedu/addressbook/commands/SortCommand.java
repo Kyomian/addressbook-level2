86f7e521e0b537f1fc4b329f7e0bfdb1803ed5b3
Jarrett <email-blocked>
2018-02-14 17:51:24 +0800

    Added SortCommand class and display texts that take into account sort

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,24 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import java.util.List;
+
+/**
+ * Sorts the address book
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = "Sorts address book according to person's name, in alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public SortCommand() {}
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonSortedShownSummary(allPersons), allPersons);
+    }
+}
